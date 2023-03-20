4f00111560d44e904c8bfb192288db1f52d6f58a
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-20 09:41:44 -0300

    configs

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import  express, { response }  from "express";
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.json({message: "Hello!"})
+})
+
+app.listen(3333, () => console.log("Server is running!"))
