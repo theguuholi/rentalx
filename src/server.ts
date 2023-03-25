3f5ac2e43a7ead2822e1fde4ea57bf00f67a8496
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-25 06:49:48 -0300

    categories

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import  express, { response }  from "express";
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message: "Hello!"})
-})
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes)
+
 
 app.listen(3333, () => console.log("Server is running!"))
