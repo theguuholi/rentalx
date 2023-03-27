e85dd7b8b44e24171b32340ca0adeebababd5540
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-27 07:43:54 -0300

    refactor modules

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+import { CategoryRepository } from "../repositories/CategoryRepository";
+import { ICategoryRepository } from "../repositories/ICategoryRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+class CreateSpecificationService {
+    constructor(private categoriesRepository: ICategoryRepository){}
+
+    execute({name, description}: IRequest): void {
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category already exists!")
+        }
+        
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export {CreateSpecificationService}
