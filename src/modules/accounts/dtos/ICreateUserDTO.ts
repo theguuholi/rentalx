0050f32f049c752277fbd95bc9b8e41fc965f6f5
Gustavo <gustavo@MacBook-Pro-de-Gustavo.local>
2023-03-29 09:54:05 -0300

    create user

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,7 @@
+interface ICreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+  driver_license: string;
+}
+export { ICreateUserDTO };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
 import { container } from "tsyringe";
+import { UsersRepositories } from "../../modules/accounts/repositories/implementations/UsersRepositories";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoryRepository } from "../../modules/cars/repositories/ICategoryRepository";
 import { CategoryRepository } from "../../modules/cars/repositories/implementations/CategoryRepository";
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
@@ -6,3 +8,4 @@ import { ISpecificationRepository } from "../../modules/cars/repositories/ISpeci
 
 container.registerSingleton<ICategoryRepository>("CategoryRepository", CategoryRepository)
 container.registerSingleton<ISpecificationRepository>("SpecificationRepository", SpecificationRepository)
+container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepositories)
