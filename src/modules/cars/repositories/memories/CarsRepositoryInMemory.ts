5fb16a8ee70c326170283405ae9ff200fb0021f3
Gus <g.92oliveira@gmail.com>
2023-04-23 08:21:33 -0300

    list cars

--- src/modules/cars/repositories/memories/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/memories/CarsRepositoryInMemory.ts
@@ -3,7 +3,6 @@ import { ICarsRepository } from "../ICarsRepository";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-
   cars: Car[] = [];
 
   async create(data: ICreateCarDTO): Promise<Car> {
@@ -11,11 +10,29 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
     Object.assign(car, data);
     this.cars.push(car);
-    return car
+    return car;
   }
 
   async findByLicensePlate(license_plate: String): Promise<Car> {
-    return this.cars.find((car) => car.license_plate === license_plate)
+    return this.cars.find((car) => car.license_plate === license_plate);
+  }
+
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string
+  ): Promise<Car[]> {
+    return await this.cars.filter((car) => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+      return null;
+    });
   }
 }
 
---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,46 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/memories/CarsRepositoryInMemory";
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRespositoryInMemory: CarsRepositoryInMemory;
+
+describe("ListCarsUseCase", () => {
+  beforeEach(() => {
+    carsRespositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRespositoryInMemory);
+  });
+
+  it("should be able to list all available cars", async () => {
+    const car = await carsRespositoryInMemory.create({
+      name: "44234234",
+      description: "Carro grande",
+      license_plate: "1231123",
+      daily_rate: 140,
+      fine_amount: 140,
+      brand: "BMW",
+      category_id: "234234",
+    });
+
+    const cars = await listCarsUseCase.execute({});
+    expect(cars).toEqual([car]);
+
+  });
+
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRespositoryInMemory.create({
+      name: "44234234",
+      description: "Carro grande",
+      license_plate: "1231123",
+      daily_rate: 140,
+      fine_amount: 140,
+      brand: "BMW",
+      category_id: "234234",
+    });
+
+    const cars = await listCarsUseCase.execute({
+        name: "44234234"
+    });
+    expect(cars).toEqual([car]);
+
+  });
+});
